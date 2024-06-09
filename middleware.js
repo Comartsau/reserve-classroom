534a9668ae3f76e56c2f372d84710a0e332de039
Comartsau <comartsau@gmail.com>
2024-06-09 23:16:40 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,15 +4,19 @@ export function middleware(request) {
   const url = request.nextUrl.clone();
   const pathname = url.pathname; // กำหนดค่า pathname จาก request.nextUrl
 
+  // ตรวจสอบว่าผู้ใช้เข้าผ่าน LIFF หรือไม่
+  const userAgent = request.headers.get("user-agent");
+  const isLine = userAgent.includes("Line");
+
   // ตรวจสอบว่า path คือ '/user' และไม่มี LIFF token จะ redirect ไปหน้า home
   if (pathname === "/user") {
-    const liffToken = request.cookies.get('liff_token');
-    if (!liffToken) {
+    const liffToken = request.cookies.get("liff_token");
+    if (!liffToken && !isLine) {
       return NextResponse.redirect(new URL("/", request.url));
     }
   }
 
-  // อนุญาตการเข้าถึงถ้า path อยู่ใน allowedPaths
+  // ตรวจสอบการอนุญาตสำหรับหน้า '/admin'
   if (pathname === "/admin") {
     const permition = "admin"; // กำหนดค่า permition ตามความต้องการ
     const allowedPaths = {
