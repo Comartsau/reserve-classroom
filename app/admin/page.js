6a3d46a5e17e3e494474897725ab46eb6f32a810
Comartsau <comartsau@gmail.com>
2024-06-10 23:18:01 +0700

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 
 function AdminHome() {
   const router = useRouter();
@@ -30,7 +31,60 @@ function AdminHome() {
     return null; // Render nothing until role is confirmed
   }
 
-  return <div>Admin</div>;
+  return   <div>
+  <AppBar position="static">
+    <Toolbar className="bg-black">
+      <div className="flex gap-3 align-middle items-center ">
+        <img
+          src="/15.jpg"
+          alt="User profile"
+          style={styles.profileImage}
+          className=" "
+        />
+        <Typography component="div" className=" text-lg ms-5">
+          warakorn Munthongsuk
+        </Typography>
+      </div>
+    </Toolbar>
+  </AppBar>
+      <div className="flex gap-3 items-center justify-between align-middle mt-3">
+        <Button variant="contained">จองห้องเรียน</Button>
+        <Button variant="contained">รายการจอง</Button>
+      </div>
+</div>
 }
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    backgroundColor: "#f0f2f5",
+    padding: "20px",
+  },
+  heading: {
+    fontSize: "2em",
+    marginBottom: "20px",
+  },
+  profileContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "20px",
+    backgroundColor: "#fff",
+    borderRadius: "10px",
+    boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+  },
+  profileImage: {
+    width: "50px",
+    height: "50px",
+    borderRadius: "50%",
+  },
+  profileDetails: {
+    textAlign: "center",
+  },
+};
 
 export default AdminHome;
