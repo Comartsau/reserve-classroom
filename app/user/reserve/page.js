4ce0363c1b62cad50fa4b00ae9de9eea5aabd0d5
Comartsau <comartsau@gmail.com>
2024-06-11 01:05:02 +0700

    Update page.js

--- app/user/reserve/page.js
+++ app/user/reserve/page.js
@@ -1,18 +1,19 @@
+'use client'
 import { Button, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 function Reserve() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleHomePage = () => {
-        router.push("/user");
-      };
+  const handleHomePage = () => {
+    router.push("/user");
+  };
 
   return (
     <div>
-        <Typography> Reserve Page</Typography>
-     
+      <Typography> Reserve Page</Typography>
+
       <Button onClick={handleHomePage}>Home Page</Button>
     </div>
   );
