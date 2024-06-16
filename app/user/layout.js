1a8d1bd530c739a5594655c540939f61bbb66ff4
Comartsau <comartsau@gmail.com>
2024-06-16 17:11:29 +0700

    Update layout.js

--- app/user/layout.js
+++ app/user/layout.js
@@ -2,7 +2,6 @@
 
 "use client";
 import { AppBar, Button, Toolbar, Typography } from "@mui/material";
-
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
@@ -13,20 +12,20 @@ const UserLayout = ({
   handleReservePage,
 }) => {
   const router = useRouter();
-  const [isAuthorized, setIsAuthorized] = useState(false);
+//   const [isAuthorized, setIsAuthorized] = useState(false);
 
-  useEffect(() => {
-    const token = sessionStorage.getItem("login");
-    if (!token) {
-      router.push("/"); // Redirect ไปที่หน้า login ถ้าไม่มี token
-    } else {
-      setIsAuthorized(true); // ตั้งค่า state ให้แสดงเนื้อหาถูกต้อง
-    }
-  }, [router]);
+//   useEffect(() => {
+//     const token = sessionStorage.getItem("login");
+//     if (!token) {
+//       router.push("/"); // Redirect ไปที่หน้า login ถ้าไม่มี token
+//     } else {
+//       setIsAuthorized(true); // ตั้งค่า state ให้แสดงเนื้อหาถูกต้อง
+//     }
+//   }, [router]);
 
-  if (!isAuthorized) {
-    return null; // ไม่แสดงเนื้อหาก่อนตรวจสอบสิทธิ์เสร็จสิ้น
-  }
+//   if (!isAuthorized) {
+//     return null; // ไม่แสดงเนื้อหาก่อนตรวจสอบสิทธิ์เสร็จสิ้น
+//   }
 
   return (
     <div className="h-screen bg-gray-300 p-2">
