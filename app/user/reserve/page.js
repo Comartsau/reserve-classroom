d8e124d3c8c131ad4c0f6386e1713776b956a05e
Comartsau <comartsau@gmail.com>
2024-06-16 17:35:39 +0700

    Update page.js

--- app/user/reserve/page.js
+++ app/user/reserve/page.js
@@ -1,54 +1,44 @@
 "use client";
-import { Button, CardContent, List, ListItem, Typography } from "@mui/material";
-import { useRouter } from "next/navigation";
-import {useEffect , useState} from "react";
+import { CardContent, List, ListItem, Typography } from "@mui/material";
 
 function Reserve() {
-  const router = useRouter();
-  const [profile, setProfile] = useState(null);
+  // useEffect(() => {
+  //   const initializeLiff = async () => {
+  //     try {
+  //       const liffId = process.env.NEXT_PUBLIC_LIFF_ID;
+  //       if (!liffId) {
+  //         throw new Error("LIFF ID is not set in environment variables");
+  //       }
+  //       await liff.init({ liffId: liffId });
+  //       if (liff.isLoggedIn()) {
+  //         const userProfile = await liff.getProfile();
+  //         setProfile(userProfile);
 
-  useEffect(() => {
-    const initializeLiff = async () => {
-      try {
-        const liffId = process.env.NEXT_PUBLIC_LIFF_ID;
-        if (!liffId) {
-          throw new Error("LIFF ID is not set in environment variables");
-        }
-        await liff.init({ liffId: liffId });
-        if (liff.isLoggedIn()) {
-          const userProfile = await liff.getProfile();
-          setProfile(userProfile);
+  //         // Set a cookie to indicate LIFF login
+  //         document.cookie = "liff_token=1; path=/";
+  //         document.cookie = "user_permition=user; path=/";
 
-          // Set a cookie to indicate LIFF login
-          document.cookie = "liff_token=1; path=/";
-          document.cookie = "user_permition=user; path=/";
+  //         // Hide the title bar in Line LIFF
+  //         if (liff.isInClient()) {
+  //           liff.ready.then(() => {
+  //             liff.hide();
+  //           });
+  //         }
+  //       } else {
+  //         liff.login();
+  //       }
+  //     } catch (error) {
+  //       console.error("LIFF Initialization failed:", error);
+  //     }
+  //   };
 
-          // Hide the title bar in Line LIFF
-          if (liff.isInClient()) {
-            liff.ready.then(() => {
-              liff.hide();
-            });
-          }
-        } else {
-          liff.login();
-        }
-      } catch (error) {
-        console.error("LIFF Initialization failed:", error);
-      }
-    };
-
-    initializeLiff();
-  }, []);
-
-  const handleHomePage = () => {
-    router.push("/user");
-  };
+  //   initializeLiff();
+  // }, []);
 
   return (
     <div>
-    <CardContent>
-        
-    <div className="bg-black rounded-sm">
+      <CardContent>
+        <div className="bg-black rounded-sm">
           <Typography
             id="modal-title"
             variant="h6"
@@ -69,7 +59,6 @@ function Reserve() {
             <ListItem>ccccc</ListItem>
           </List>
         </div>
-
       </CardContent>
     </div>
   );
