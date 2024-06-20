2877b394df3762154930ffc75f8d03d7670e6521
Comartsau <comartsau@gmail.com>
2024-06-20 15:23:40 +0700

    Update page.js

--- app/user/reserve/page.js
+++ app/user/reserve/page.js
@@ -103,38 +103,38 @@ function Reserve() {
   };
 
 
-  // useEffect(() => {
-  //   const initializeLiff = async () => {
-  //     try {
-  //       const liffId = process.env.NEXT_PUBLIC_LIFF_ID;
-  //       if (!liffId) {
-  //         throw new Error("LIFF ID is not set in environment variables");
-  //       }
-  //       await liff.init({ liffId: liffId });
-  //       if (liff.isLoggedIn()) {
-  //         const userProfile = await liff.getProfile();
-  //         setProfile(userProfile);
+  useEffect(() => {
+    const initializeLiff = async () => {
+      try {
+        const liffId = process.env.NEXT_PUBLIC_LIFF_ID;
+        if (!liffId) {
+          throw new Error("LIFF ID is not set in environment variables");
+        }
+        await liff.init({ liffId: liffId });
+        if (liff.isLoggedIn()) {
+          const userProfile = await liff.getProfile();
+          setProfile(userProfile);
 
-  //         // Set a cookie to indicate LIFF login
-  //         document.cookie = "liff_token=1; path=/";
-  //         document.cookie = "user_permition=user; path=/";
+          // Set a cookie to indicate LIFF login
+          document.cookie = "liff_token=1; path=/";
+          document.cookie = "user_permition=user; path=/";
 
-  //         // Hide the title bar in Line LIFF
-  //         if (liff.isInClient()) {
-  //           liff.ready.then(() => {
-  //             liff.hide();
-  //           });
-  //         }
-  //       } else {
-  //         liff.login();
-  //       }
-  //     } catch (error) {
-  //       console.error("LIFF Initialization failed:", error);
-  //     }
-  //   };
+          // Hide the title bar in Line LIFF
+          if (liff.isInClient()) {
+            liff.ready.then(() => {
+              liff.hide();
+            });
+          }
+        } else {
+          liff.login();
+        }
+      } catch (error) {
+        console.error("LIFF Initialization failed:", error);
+      }
+    };
 
-  //   initializeLiff();
-  // }, []);
+    initializeLiff();
+  }, []);
 
 
   return (
