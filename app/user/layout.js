07f715162eecbafc62fc082d5ff447af61fa4300
Comartsau <comartsau@gmail.com>
2024-06-20 11:58:27 +0700

    Update layout.js

--- app/user/layout.js
+++ app/user/layout.js
@@ -84,13 +84,12 @@ const UserLayout = ({ children }) => {
       <AppBar position="static">
         <Toolbar className="bg-black">
           <div className="flex gap-3 align-middle items-center">
-            {/* <img
-              src={profile.pictureUrl  || ''}
-              alt="User profile"
-              style={styles.profileImage}
-            /> */}
             {profile?.pictureUrl && (
-              <img src={profile?.pictureUrl} alt="Profile Picture" />
+              <img
+                src={profile?.pictureUrl}
+                alt="User Profile"
+                style={styles.profileImage}
+              />
             )}
             <Typography component="div" className="text-lg ms-5">
               {profile?.displayName}
