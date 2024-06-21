7ba4b07dfa5e506b12808a0532ee4d27631b4d9c
Comartsau <comartsau@gmail.com>
2024-06-21 16:48:45 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Login from "./components/auth/login";
 
 export default function Home() {
   return (
-    <div className="bg-gray-300 h-screen">
+    <div className="bg-gray-300 ">
       <Login />
     </div>
   );
