3cebe6aaceb353b0ec0db347f4ec5970678a48e7
Comartsau <comartsau@gmail.com>
2024-07-19 23:22:10 +0700

    Update viewReportModal.jsx

--- app/components/viewReportModal.jsx
+++ app/components/viewReportModal.jsx
@@ -44,7 +44,7 @@ const ViewReportModal = ({ open, onClose, onView, modalStyleCreate, data }) => {
                     </div>
                     <div className="flex items-center gap-3">
                       <div className="flex items-center">{`${item?.trade}`}</div>
-                      <div className="flex items-center whitespace-nowrap">{`${item?.name1}`} </div>
+                      <div className="flex items-center whitespace-nowrap">{`${item?.full_name}`} </div>
                       <div className="flex items-center whitespace-nowrap">{`${item?.address}`}</div>
                     </div>
                   </div>
